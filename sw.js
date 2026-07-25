be87ba7865f5a8e17d2b71b08aeb737518c0fb5b
Heedzalo <tabbnz1@gmail.com>
2026-07-25 17:59:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener('notificationclick', (event) => {
       for(const client of clientList){
         if('focus' in client) return client.focus();
       }
-      if(self.clients.openWindow) return self.clients.openWindow('/');
+      if(self.clients.openWindow) return self.clients.openWindow('./');
     })
   );
 });
